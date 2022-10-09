2d2277749ab768bf207ccca9547518ec11acae94
Thomas Crowe <wcrowe@g.clemson.edu>
2022-10-09 12:38:57 -0400

    Added images, navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 
 import { About, Footer, Header, Skills, Testimonials, Work } from './containers';
 import { Navbar } from './components';
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import './Navbar.scss'
+import { images } from '../../constants'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [Toggle, setToggle] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img className="app__navbar-img" src={images.thomas} alt="logo"/>
+      </div>
+      <ul className="app__navbar-links">
+        {['Home', 'About', 'Skills', 'Work', 'Testimonials', 'Contact'].map((item, index) => (
+          <li className="app__flex p-text" key={index}>
+            <div />
+            <a href={`#${item.toLowerCase()}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)}/>
+        {Toggle && (
+          <motion.div 
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'ease-out' }}
+            >
+              <HiX onClick={() => setToggle(false)}/>
+              <ul className="app__navbar-links">
+                {['Home', 'About', 'Skills', 'Work', 'Testimonials', 'Contact'].map((item, index) => (
+                  <li className="app__flex p-text" key={index}>
+                    <a href={`#${item.toLowerCase()}`} onClick={() => setToggle(false)}>{item}</a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+        )}
+      
+      </div>
+
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
